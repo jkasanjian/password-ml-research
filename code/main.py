592d76e19d1ddd8ef3e9676cca6f9041c3c5d332
Josh <jkasanjian@gmail.com>
2020-04-16 14:10:08 -0700

    each subject data graphed

--- code/main.py
+++ code/main.py
@@ -2,68 +2,96 @@ import csv
 import json
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+
+
+
+DATA_SOURCE = 'data/DSL-StrongPasswordData.csv'
+DATA_JSON = 'data/password_data.json'
+USER_GRAPHS = 'data/graphs/'
+
+
+def make_json():
+    data = {}
+    with open(DATA_SOURCE) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            if row[0] not in data:
+                trials = []
+                trials.append(row[3:])
+                data[row[0]] = trials 
+            else:
+                data[row[0]].append(row[3:])
+
+    with open(DATA_JSON, 'w') as fp:
+        json.dump(data, fp)
+
+
+def load_datasets(subject, data):
+    ''' Loads train and test datasets for a given subject 
+        train: first 200 repititions of user
+        test_user: last 200 repititions of user
+    ''' 
+    train = data[subject][:200]
+    test_user = data[subject][200:]
+    test_imposter = []
+    for s in data:
+        if s != subject:
+            test_imposter.extend(data[s][:5])
+
+    train = convert_to_float(train)
+    test_user = convert_to_float(test_user)
+    test_imposter = convert_to_float(test_imposter)
+
+    return train, test_user, test_imposter    
+
+
+def convert_to_float(rows):
+    for i in range(len(rows)):
+        rows[i] = [float(r) for r in rows[i]]
+    return rows
+
 
 
 def signature_graph():
+    ''' Creates visualization of each subject ''' 
+    with open(DATA_JSON) as json_file:
+        data = json.load(json_file)
 
-    with open('data/individual-user-datum/s002.csv', 'r') as data:
-        plots= csv.reader(data, delimiter=',')
-        is_title_row = True
-        row_count = 0
-        data_list = []
-        y_axis = []
+    features = data['subject'][0]
+    del data['subject']
 
-        for row in plots:
+    x = [i for i in range(len(features))]
+    for subject in data:
+        plt.figure(figsize=(16,8))
+        for trial in data[subject]:
+            plt.scatter(x, trial)
+        plt.xticks(x, features, rotation='vertical', fontsize=10)
+        plt.margins(0.2)
+        plt.subplots_adjust(bottom=0.2)
+        plt.xlim(-1,31)
+        plt.grid(True)
+        plt.xlabel('Features')
+        plt.ylabel('Time')
+        plt.title(subject)
+        f_name = USER_GRAPHS + subject
+        plt.savefig(f_name)
+        plt.close()
 
-            # Row is just column names
-            if(is_title_row):
-                print(row)
-                title_list = row
-                is_title_row = False
-                row_count += 1
-                print("\n\nActual data:")
-            else:
-                data_list.append(row[3:])
-                row_count += 1
-           
-        
-        for i in range(len(title_list)):
-            print(data_list[i])
-            for j in range(len(data_list[0])):
-                z = 0 # Debug val
 
-            
-   
-    # plt.plot(x,y, marker='o')
 
-    # plt.title('s002 Typing Signature')
+def test():
+    with open(DATA_JSON) as json_file:
+        data = json.load(json_file)
 
-    #x,y = np.loadtxt('data/individual-user-datum/s002.csv', unpack = True, delimiter = ',')
-    my_data = np.genfromtxt('data/individual-user-datum/s002.csv', delimiter=',')
-    print(my_data)
+    subjects = data.keys()
+
+    train, test_user, test_imposter = load_datasets(subjects[0], data)
     
-    #plt.plot(x,y)
-    #plt.show()
-    
-    # with open('data/individual-user-datum/s002.csv', 'r') as data:
-    #     plots= csv.reader(data, delimiter=',')
-    #     is_title_row = True
-    #     row_count = 0
-    #     for row in plots:
 
-    #         # Row is just column names
-    #         if(is_title_row):
-    #             print(row)
-    #             title_list = row
-    #             is_title_row = False
-    #             row_count += 1
-    #             print("\n\nActual data:")
-    #         else:
-    #             print(row[3:]) # Cut off first 3 columns
-    #             row_count += 1
 
 
 
 if __name__ == '__main__':
     signature_graph()
+
+
