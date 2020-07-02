1f01cb7692dc88ef0f81bd1775ca177ff5a5b7a3
Joshua Kasanjian <jkasanjian@gmail.com>
2020-07-02 02:00:48 -0700

    testing with f1 score attempt 1

--- code/main.py
+++ code/main.py
@@ -36,7 +36,11 @@ RESULTS_ALONE = 'results/alone/'
 RESULTS_COMBO = 'results/combo/'
 
 
+
+
+###########################################################################
 # ---------------------------- DATA PROCESSING ----------------------------
+###########################################################################
 
 def read_data():
     with open(DATA_JSON) as json_file:
@@ -44,13 +48,12 @@ def read_data():
     # features = data['subject']
     del data['subject']
     subjects = list(data.keys())
-    
     return data, subjects
+
 
 def get_features():
     with open(DATA_JSON) as json_file:
         data = json.load(json_file)
-
     return data['subject'][0]
 
 
@@ -68,7 +71,6 @@ def make_json():
 
     with open(DATA_JSON, 'w') as fp:
         json.dump(data, fp)
-
 
 
 def create_signature_graphs():
@@ -97,7 +99,6 @@ def create_signature_graphs():
         plt.close()
 
 
-
 def load_all_data(subject, data):
     '''
     Returns all data as x, y given a subject
@@ -107,13 +108,12 @@ def load_all_data(subject, data):
     imposter = []
     for s in data:
         if s != subject:
-            imposter.extend(data[s][:10])
+            imposter.extend(data[s][:12])
 
     x = user + imposter
     y = [1 for i in range(len(user))] + [-1 for i in range(len(imposter))]
 
     return (np.array(x).astype(np.float64), np.array(y).astype(np.float64))
-
 
 
 def split_data_sklearn():
@@ -122,12 +122,11 @@ def split_data_sklearn():
         mkdir(DATA_SPLIT_SK + s)
 
         x, y = load_all_data(s, data)
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5, train_size=0.5)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, train_size=0.6)
         np.save(DATA_SPLIT_SK + s + '/x_train.npy', x_train)
         np.save(DATA_SPLIT_SK + s + '/x_test.npy', x_test)
         np.save(DATA_SPLIT_SK + s + '/y_train.npy', y_train)
         np.save(DATA_SPLIT_SK + s + '/y_test.npy', y_test)
-
 
 
 def load_data_sklearn(s):
@@ -136,166 +135,74 @@ def load_data_sklearn(s):
     x_test = np.load(DATA_SPLIT_SK + s + '/x_test.npy')
     y_train = np.load(DATA_SPLIT_SK + s + '/y_train.npy')
     y_test = np.load(DATA_SPLIT_SK + s + '/y_test.npy')
-
     return x_train, x_test, y_train, y_test
     
-
-
-def load_models(s):
-    ''' Returns all models for a given subject s
-        ADD NEW MODELS HERE TO RETURN STATEMENT '''
-    knn_clf = load(MODELS_KNN + s + '.joblib') 
-    log_clf = load(MODELS_LOGIT + s + '.joblib')
-    rfr_clf = load(MODELS_RF + s +'.joblib')
-
-    return knn_clf, log_clf, rfr_clf
-
 
 def load_model(name, s):
     ''' Returns the corresponding model for a subject '''
     return load('models/' + name + '/' + s + '.joblib')
 
 
-def get_params_rf(s):
-    ''' Returns the calculate best hyperparameters for a subject's
-        Random Forest model '''
-    with open(MODELS_RF+'best_params.json') as json_file:
-        params = json.load(json_file)
-    return params[s]
 
-
-def get_params_knn(s):
-    ''' Returns the calculate best hyperparameters for a subject's
-        Random Forest model '''
-    with open(MODELS_KNN+'best_params.json') as json_file:
-        params = json.load(json_file)
-    return params[s]
-
-
-def get_params_logreg(s):
-    ''' Returns the calculate best hyperparameters for a subject's
-        Random Forest model '''
-    with open(MODELS_LOGIT+'best_params.json') as json_file:
-        params = json.load(json_file)
-    return params[s]
-
-
-def get_params_svm(s):
-    ''' Returns the calculate best hyperparameters for a subject's
-        Random Forest model '''
-    with open(MODELS_SVM+'best_params.json') as json_file:
-        params = json.load(json_file)
-    return params[s]
-
-
+###############################################################
 # ---------------------------- KNN ----------------------------
+###############################################################
 
-def knn_tuning():
+def knn_training():
     ''' Calculates and saves the best hyperparameteres for each subject's KNN model '''
     _, subjects = read_data()
-    best_params = {}
-
 
     leaf_size = list(range(1,50))
     n_neighbors = list(range(1,30))
-    p=[1,2]
+    p = [1,2]
     hyperparameters = dict(leaf_size=leaf_size, n_neighbors=n_neighbors, p=p)
 
     for s in subjects:
         x_train, _, y_train, _ = load_data_sklearn(s)
 
-        knn_tune = KNeighborsClassifier()
-        clf = GridSearchCV(knn_tune, hyperparameters, cv=10)
-        clf.fit(x_train, y_train)
-        
-        best_params[s] = clf.best_params_
-    
-    with open(MODELS_KNN+'best_params.json', 'w') as fp:
-        json.dump(best_params, fp)
-
-
-def knn_classifier():
-    ''' Generates a KNN model for each subject, stored in models/knn/ folder'''
-    _, subjects = read_data()
-
-    for s in subjects:
-        x_train, _, y_train, _ = load_data_sklearn(s)
-        params = get_params_knn(s)
-        clf = KNeighborsClassifier(algorithm='brute', metric='minkowski')
-        clf.set_params(**params)
+        knn_tune = KNeighborsClassifier(algorithm='brute', metric='minkowski')
+        clf = GridSearchCV(knn_tune, hyperparameters, scoring='f1', n_jobs=-1)
         clf.fit(x_train, y_train)
 
         dump(clf, MODELS_KNN + s + '.joblib')
+        
 
 
 
+###############################################################################
 # ---------------------------- Logistic Regression ----------------------------
+###############################################################################
 
-def log_reg_tuning():
+def log_reg_training():
     '''' Calculates and saves the best hyperparameteres for each subject's
     Logistic Regression model '''
     _, subjects = read_data()
-    best_params = {}
     
-    param_grid = [    
-        {'penalty' : ['l1', 'l2', 'elasticnet', 'none'],
-        'C' : np.logspace(-4, 4, 20),
-        'solver' : ['lbfgs','newton-cg','liblinear','sag','saga'],
-        'max_iter' : [100, 1000, 2500, 5000]
-        }
-    ]
+    penalty = ['l1', 'l2', 'elasticnet', 'none']
+    C = np.logspace(-4, 4, 20)
+    solver = ['lbfgs','newton-cg','liblinear','sag','saga']
+    max_iter = [100, 1000, 2500, 5000]
+    hyperparameteres = dict(penalty=penalty, C=C, solver=solver, max_iter=max_iter)
 
     for s in subjects:
         x_train, _, y_train, _ = load_data_sklearn(s)
         
-        log_model = LogisticRegression()
-        clf = GridSearchCV(log_model, param_grid = param_grid, cv = 3, verbose=True, n_jobs=-1)
+        log_tune = LogisticRegression()
+        clf = GridSearchCV(log_tune, hyperparameteres, scoring='f1', n_jobs=-1)
         clf.fit(x_train, y_train)
-        best_params[s] = clf.best_params_
-
-    with open(MODELS_LOGIT+'best_params.json', 'w') as fp:
-        json.dump(best_params, fp)
-
-
-
-def log_reg_classifier():
-    ''' Generates a Logistic regression model for each subject, 
-        stored in models/logistic/ folder '''
-    _, subjects = read_data()
-
-    for s in subjects:
-        x_train, _, y_train, _ = load_data_sklearn(s) 
-        clf = LogisticRegression()
-        params = get_params_logreg(s)
-        clf.set_params(**params)
-        clf.fit(x_train, y_train)
-
+       
         dump(clf, MODELS_LOGIT + s + '.joblib')
 
 
 
+#########################################################################
 # ---------------------------- Random Forest ----------------------------
+#########################################################################
 
-def random_forest_classifier():
-    ''' Generates a Random Forest classifier model for each 
-        subject stored in models/rfc/ folder '''
-    _, subjects = read_data()
-    
-    for s in subjects:
-        x_train, _, y_train, _ = load_data_sklearn(s)
-        params = get_params_rf(s)
-        clf = RandomForestRegressor(**params)
-        clf.fit(x_train, y_train)
-        dump(clf, MODELS_RF + s + '.joblib')
-        #visualize_tree(clf, s)
-
-
-
-def random_forest_classifier_tuning():
+def random_forest_training():
     ''' Generates a Random Forest regression model for each subject'''
     _, subjects = read_data()
-    best = {}
-
+   
     n_estimators = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
     max_features = ['auto', 'sqrt']
     max_depth = list(range(50, 100, 10))
@@ -303,33 +210,23 @@ def random_forest_classifier_tuning():
     min_samples_split = [2, 5, 10]
     min_samples_leaf = [1, 2, 4]
     bootstrap = [True, False]
-
-    param_dist = {'n_estimators': n_estimators,
-                'max_features': max_features,
-                'max_depth': max_depth,
-                'min_samples_split': min_samples_split,
-                'min_samples_leaf': min_samples_leaf,
-                'bootstrap': bootstrap}
+    hyperparameteres = dict(
+                n_estimators=n_estimators,
+                max_features=max_features,
+                max_depth=max_depth,
+                min_samples_split=min_samples_split,
+                min_samples_leaf=min_samples_leaf,
+                bootstrap=bootstrap)
 
     for s in subjects:
-        x_train, x_test, y_train, y_test = load_data_sklearn(s) 
-        # Create the random grid
-        print(s)
-        break
-        rf = RandomForestClassifier()
-        rf = GridSearchCV(estimator=rf, param_grid=param_dist, cv=3, scoring='precision_macro', n_jobs= -1)
-        rf.fit(x_train, y_train)
+        x_train, _, y_train, _ = load_data_sklearn(s) 
 
-        best[s] = rf.best_params_
-        print('DONE', rf.best_params_)
-        # break
-        y_pred = rf.predict(x_test)
-        print(metrics.classification_report(y_test, y_pred))
-        break
-
-    # with open(MODELS_RF+'best_params.json', 'w') as fp:
-    #     json.dump(best, fp)
-
+        rf_tune = RandomForestClassifier()
+        clf = GridSearchCV(rf_tune, hyperparameteres, scoring='f1', n_jobs= -1)
+        clf.fit(x_train, y_train)
+        
+        dump(clf, MODELS_RF + s + '.joblib')
+        # vizualize_tree(clf, s)
 
 
 def visualize_tree(clf, s):
@@ -348,66 +245,63 @@ def visualize_tree(clf, s):
 
 
 
-
+###############################################################
 # ---------------------------- SVM ----------------------------
+###############################################################
 
-def support_vector_tuning():
+
+def svm_training():
     ''' Calculates and saves the best hyperparameteres for each subject's SVM model '''
     _, subjects = read_data()
-    best_params = {}
-    param_grid = {"kernel":["linear","rbf","poly"], "C" : [i for i in range(1,1000,10)] , "degree": [1,2]}
+
+    kernel = ["linear","rbf","poly"]
+    C = [i for i in range(1,1000,10)]
+    degree = [1,2]
+
+    hyperparameteres = dict(kernel=kernel, C=C, degree=degree)
     
     for s in subjects:
         x_train, _, y_train, _= load_data_sklearn(s)
-        
-        grid_classifier = GridSearchCV(SVC(gamma ="auto",decision_function_shape="ovo"), param_grid, scoring = 'recall_macro', cv = 5)
-        grid_classifier.fit(x_train,y_train)
 
-        best_params[s] = (grid_classifier.best_params_)
-
-    with open(MODELS_SVM+'best_params.json', 'w') as fp:
-        json.dump(best_params, fp)
-
-
-
-def support_vector_classifier():
-    ''' Generates a SVM classifier model for each sbuject
-        stored in models/svm/ folder '''
-    _, subjects = read_data()
-    
-    for s in subjects:
-        x_train, _, y_train, _ = load_data_sklearn(s)
-
-        params = get_params_svm(s)
-        clf = SVC(gamma ="auto",decision_function_shape="ovo")
-        clf.set_params(**params)
+        svm_tune = SVC(gamma ='auto',decision_function_shape='ovo')
+        clf = GridSearchCV(svm_tune, hyperparameteres, scoring='f1', n_jobs=-1)
         clf.fit(x_train, y_train)
 
         dump(clf, MODELS_SVM + s + '.joblib')
 
 
 
+######################################################################
 # ---------------------------- EVALUATION ----------------------------
+######################################################################
 
 def evaluate_all_models_alone():
     ''' Evaluates all models alone and generates results files '''
-    models = [('knn', False), ('logistic', False), ('randfor', True), ('svm', False)]
+    models = ('knn', 'logistic', 'randfor', 'svm')
     for m in models:
-        evaluate_model(m[0], m[1])
+        evaluate_model(m)
 
+    
+def run_majority_votes():
+    models = [  ['knn', 'logistic', 'randfor'],
+                ['knn', 'logistic', 'svm'],
+                ['knn', 'randfor', 'svm'],
+                ['logistic', 'randfor', 'svm'] ]
+    
+    for m_set in models:
+        evaluate_majority_vote(m_set)
 
-
-def evaluate_model(name, check_pred):
+def evaluate_model(name):
     ''' _Evaluates the model named by calculating the false rejection and false acceptance rates 
-        Params: name - name of model, check_pred - True if model returns probability not label '''
+        Params: name - name of model '''
     _, subjects = read_data()
     results = {}
     results['subjects'] = {}
     all_miss_rate = []
     all_f_alarm_rate = []
-    all_accs = []
     all_pers = []
     all_recs = []
+    all_f1s = []
 
     for s in subjects:
         _, x_test, _, y_test = load_data_sklearn(s)
@@ -422,8 +316,6 @@ def evaluate_model(name, check_pred):
         y_pred = clf.predict(x_test)
 
         for i in range(n):
-            if check_pred:
-                y_pred = np.where(y_pred >= 0.5, 1.0, -1.0)
 
             if y_test[i] == 1.0:
                 total_pos += 1
@@ -437,7 +329,7 @@ def evaluate_model(name, check_pred):
         false_alarm_rate = f_a / n
         precision = (total_pos - miss) / ((total_pos - miss) + miss)
         recall = (total_pos - miss) / ((total_pos - miss) + f_a)
-        accuracy = (n - miss - f_a) / n 
+        f1 = 2 * ((precision * recall) / (precision + recall))
 
         s_results['false acceptance rate'] = miss_rate
         all_miss_rate.append(miss_rate)
@@ -447,8 +339,8 @@ def evaluate_model(name, check_pred):
         all_pers.append(precision)
         s_results['recall'] = recall
         all_recs.append(recall)
-        s_results['accuracy'] = accuracy
-        all_accs.append(accuracy)
+        s_results['f1'] = f1
+        all_f1s.append(f1)
         
         results['subjects'][s] = s_results
         
@@ -460,11 +352,13 @@ def evaluate_model(name, check_pred):
     results['precision SD'] = np.array(all_pers).std()
     results['recall mean'] = np.array(all_recs).mean()
     results['recall SD'] = np.array(all_recs).std()
-    results['accuracy mean'] = np.array(all_accs).mean()
-    results['accuracy SD'] = np.array(all_accs).std()
+    results['f1 mean'] = np.array(all_f1s).mean()
+    results['f1 SD'] = np.array(all_f1s).std()
+    
     
     with open(RESULTS_ALONE + name + '.json', 'w') as fp:
         json.dump(results, fp)
+
 
 
 def evaluate_majority_vote(models):
@@ -476,18 +370,18 @@ def evaluate_majority_vote(models):
     results['subjects'] = {}
     all_miss_rate = []
     all_f_alarm_rate = []
-    all_accs = []
     all_pers = []
     all_recs = []
+    all_f1s = []
     fname = RESULTS_COMBO
 
     first = True
     for s in subjects:
         clfs = []
         for m in models:
-            clfs.append(load_model(m[0], s))
+            clfs.append(load_model(m, s))
             if first:
-                fname += m[0] + '-'
+                fname += m + '-'
         first = False
         
         _, x_test, _, y_test = load_data_sklearn(s)
@@ -533,7 +427,7 @@ def evaluate_majority_vote(models):
         false_alarm_rate = f_a / n
         precision = (total_pos - miss) / ((total_pos - miss) + miss)
         recall = (total_pos - miss) / ((total_pos - miss) + f_a)
-        accuracy = (n - miss - f_a) / n 
+        f1 = 2 * ((precision * recall) / (precision + recall))
 
         s_results['false acceptance rate'] = miss_rate
         all_miss_rate.append(miss_rate)
@@ -543,8 +437,8 @@ def evaluate_majority_vote(models):
         all_pers.append(precision)
         s_results['recall'] = recall
         all_recs.append(recall)
-        s_results['accuracy'] = accuracy
-        all_accs.append(accuracy)
+        s_results['f1'] = f1
+        all_f1s.append(f1)
         
         results['subjects'][s] = s_results
         
@@ -556,8 +450,8 @@ def evaluate_majority_vote(models):
     results['precision SD'] = np.array(all_pers).std()
     results['recall mean'] = np.array(all_recs).mean()
     results['recall SD'] = np.array(all_recs).std()
-    results['accuracy mean'] = np.array(all_accs).mean()
-    results['accuracy SD'] = np.array(all_accs).std()
+    results['f1 mean'] = np.array(all_f1s).mean()
+    results['f1 SD'] = np.array(all_f1s).std()
     results['win count'] = win_count
     
     fname = fname[:-1] + '.json'
@@ -565,22 +459,7 @@ def evaluate_majority_vote(models):
         json.dump(results, fp)
             
             
-    
-def run_majority_votes():
-    # models = 
-    models = [  [('knn', False), ('logistic', False), ('randfor', True)],
-                [('knn', False), ('logistic', False), ('svm', False)],
-                [('knn', False), ('randfor', True), ('svm', False)],
-                [('logistic', False), ('randfor', True), ('svm', True)] ]
-    
-    for m_set in models:
-        evaluate_majority_vote(m_set)
-
-
 def print_all_stats():
-    # for f in listdir('results/alone/'):
-    #     file = 'results/alone/' + f
-
     dash = '-' * 40
     for path, _, files in os.walk('results'):
         for name in files:
@@ -591,6 +470,9 @@ def print_all_stats():
                     print('\n\n\nResults for', name[:-5])
                     print(dash)
 
+                    print('{:<30s}{:<2.8f}'.format('F1 mean:', 100*data['f1 mean']))
+                    print('{:<30s}{:<2.8f}'.format('F1 SD:', 100*data['f1 SD']))
+                    print()
                     print('{:<30s}{:<2.8f}'.format('Precision mean:', 100*data['precision mean']))
                     print('{:<30s}{:<2.8f}'.format('Precision SD:', 100*data['precision SD']))
                     print()
@@ -603,16 +485,24 @@ def print_all_stats():
                     print('{:<30s}{:<2.8f}'.format('FRR mean:', 100*data['false rejection rate mean']))
                     print('{:<30s}{:<2.8f}'.format('FRR SD:', 100*data['false rejection rate SD']))
                     
-                    
-                    
-                    
-                    
-                   
 
 
 
+
+################################################################
+# ---------------------------- MAIN ----------------------------
+################################################################
 if __name__ == '__main__':
-    # evaluate_all_models_alone()
-    # run_majority_votes()
-    # print_all_stats()
-    random_forest_classifier_tuning()
+    knn_training()
+    print('KNN done training')
+    log_reg_training()
+    print('Logistic regression done training')
+    random_forest_training()
+    print('Random forest done training')
+    svm_training()
+    print('SVM done training')
+
+    evaluate_all_models_alone()
+    run_majority_votes()
+    print_all_stats()
+    
