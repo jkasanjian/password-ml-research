ae060fb4befd0e6ff8517161dc5f5c5b0fa37d39
Joshua Kasanjian <jkasanjian@gmail.com>
2020-10-11 22:50:30 -0700

    partitioned data

---
+++ code/data_processing.py
@@ -0,0 +1,72 @@
+from os import path, mkdir
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+
+DATA_PARTITIONS = "data/partitions/"
+
+
+def read_data():
+    with open(path.abspath("data/password_data.json")) as json_file:
+        data = json.load(json_file)
+    labels = data["subject"]
+    del data["subject"]
+    return data, labels
+
+
+def get_pos_neg(data, subject):
+    """ Returns positive and negative data for a given subject"""
+    x_pos = data[subject]
+    x_neg = []
+    for s in data:
+        if s != subject:
+            x_neg.extend(data[s])
+    return x_pos, x_neg
+
+
+def partition_data_all():
+    """ Partitions the data using all datapoints and saves in files """
+    data, _ = read_data()
+
+    for s in data:
+        x_pos, x_neg = get_pos_neg(data, s)
+        x = np.array(x_pos + x_neg)
+        y = np.array([1 for i in range(len(x_pos))] + [-1 for i in range(len(x_neg))])
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30)
+
+        if not path.isdir(DATA_PARTITIONS + "all_data/" + s):
+            mkdir(DATA_PARTITIONS + "all_data/" + s)
+
+        np.save(DATA_PARTITIONS + "all_data/" + s + "/x_train.npy", x_train)
+        np.save(DATA_PARTITIONS + "all_data/" + s + "/x_test.npy", x_test)
+        np.save(DATA_PARTITIONS + "all_data/" + s + "/y_train.npy", y_train)
+        np.save(DATA_PARTITIONS + "all_data/" + s + "/y_test.npy", y_test)
+
+
+def partition_data_balanced():
+    """ Partitions the data in class balanced form and saves in files """
+    data, _ = read_data()
+
+    for s in data:
+        x_pos, x_neg = get_pos_neg(data, s)
+
+        # TODO: make balanced
+        x = np.array(x_pos + x_neg)
+        y = np.array([1 for i in range(len(x_pos))] + [-1 for i in range(len(x_neg))])
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30)
+
+        if not path.isdir(DATA_PARTITIONS + "all_data/" + s):
+            mkdir(DATA_PARTITIONS + "all_data/" + s)
+
+        np.save(DATA_PARTITIONS + "all_data/" + s + "/x_train.npy", x_train)
+        np.save(DATA_PARTITIONS + "all_data/" + s + "/x_test.npy", x_test)
+        np.save(DATA_PARTITIONS + "all_data/" + s + "/y_train.npy", y_train)
+        np.save(DATA_PARTITIONS + "all_data/" + s + "/y_test.npy", y_test)
+
+
+if __name__ == "__main__":
+    """ Main method """
+    partition_data_all()
